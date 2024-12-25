f869f4aac8ed0c77bbe17185a48f9394c6a473db
Eric <ericrwang2011@gmail.com>
2024-12-25 10:28:06 -0600

    Initial Commit

---
+++ joyRide/joyRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  joyRideApp.swift
+//  joyRide
+//
+//  Created by Eric Wang on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct joyRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
